3300ac955db10d3da30804214324656fc235d5b6
rlyon01 <rchrdlyon1@gmail.com>
2023-04-27 20:24:45 +1000

    Added performance timing.

--- first_tan_poly.py
+++ first_tan_poly.py
@@ -15,33 +15,29 @@ Usage:
   >>> main()
 """
 
+from time import process_time
 from math import tan, pi
-from first_algorithm import remez_poly
+from remes_first_algorithm import remez_poly
 from utility import plot_result
 
 def main() -> None:
   """Polynomial approximation of TAN function"""
   print('Polynomial approximation of TAN function')
-  # define the function to be approximated
-  f = tan
-  # lower limit on interval of approximation
-  lower = 0.0
-  # upper limit on interval of approximation
-  upper = 0.25 * pi
-  # number of grid points in the interval
-  num = 51
-  # maximum number of iterations
-  mit = 10
-  # polynomial order
-  order = 5
-
+  f = tan             # define the function to be approximated
+  lower = 0.0         # lower limit on interval of approximation
+  upper = 0.25*pi     # upper limit on interval of approximation
+  num = 51            # number of grid points in the interval
+  mit = 10            # maximum number of iterations
+  order = 5           # polynomial order
   # calculate the best approximation on grid
+  start_time = process_time()
   coefficients, error, it = remez_poly(f, lower, upper, num, order, mit)
-
+  end_time = process_time()
   # display the results
   print(f"Coefficients: [{', '.join([f'{c:.15e}' for c in coefficients])}]")
   print(f'Error: {error:.6e}')
   print(f'Iterations: {it}')
+  print(f'Duration: {end_time-start_time:.4f} sec')
   # plot the results
   plot_result(f, coefficients, lower, upper, num,
     'Polynomial Approximation of TAN function')
