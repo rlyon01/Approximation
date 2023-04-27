8b6fa61cf1ec0a446b4b737b083b3c53b31fc4c2
rlyon01 <rchrdlyon1@gmail.com>
2023-04-27 20:14:27 +1000

    Add performance timing.

--- first_clipped_sine_poly.py
+++ first_clipped_sine_poly.py
@@ -1,8 +1,8 @@
 """ Test the polynomial approximation for a clipped sine function.
 
 This test uses Remez's first algorithm as implemented in the module
-first_algorithm. A discrete grid over the interval [-pi, pi] with 4096 points is
-used. The polynomial order is set to 17.
+remes_first_algorithm. A discrete grid over the interval [-pi, pi] with 4096
+points is used. The polynomial order is set to 17.
 
 The clipped sine is defined by the nested function:
 
@@ -14,9 +14,6 @@ The clipped sine is defined by the nested function:
         y = -0.7071067811865475
       return y
 
-The algorithm is completed after 55 iterations. The resulting approximation
-has a maximum error of 2.364630e-02.
-
 Module:
   first_clipped_sine_poly
 
@@ -25,39 +22,37 @@ Usage:
   >>> main()
 """
 
+from time import process_time
 from math import sin, pi
-from first_algorithm import remez_poly
+from remes_first_algorithm import remez_poly
 from utility import plot_result
 
 def main() -> None:
   """Polynomial approximation of Clipped-Sine function"""
   print('Polynomial approximation of Clipped-Sine function')
-  # The clipped sine function
   def clipped_sine(x : float) -> float:
+    """The clipped sine function."""
     y = sin(x)
     if y > 0.7071067811865475:
       y = 0.7071067811865475
     elif y < -0.7071067811865475:
       y = -0.7071067811865475
     return y
-
-  # lower limit on interval of approximation
-  lower = -pi
-  # upper limit on interval of approximation
-  upper = pi
-  # number of grid points in the interval
-  num = 4096
-  # maximum number of iterations
-  mit = 100
-  # polynomial order
-  order = 17
+  lower = -pi   # lower limit on interval of approximation
+  upper = pi    # upper limit on interval of approximation
+  num = 4096    # number of grid points in the interval
+  mit = 100     # maximum number of iterations
+  order = 17    # polynomial order
   # calculate the best approximation on grid
+  start_time = process_time()
   coefficients, error, it = remez_poly(clipped_sine, lower, upper, num,
     order, mit)
+  end_time = process_time()
   # display the results
   print(f"Coefficients: [{', '.join([f'{c:.15e}' for c in coefficients])}]")
   print(f'Error: {error:.6e}')
   print(f'Iterations: {it}')
+  print(f'Duration: {end_time-start_time:.4f} sec')
   # plot results
   plot_result(clipped_sine, coefficients, lower, upper, num,
     'Polynomial Approximation of Clipped-Sine function')
